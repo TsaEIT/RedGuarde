9314d844cb6d8649f3e094041e87ed92998f1bf2
TSAEit <tjm27@scasd.org>
2019-03-06 17:51:22 -0500

    Fixed a LOT of bugs, cause some of the files seem to be important.

---
+++ data/map/path.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="path" tilewidth="16" tileheight="16" tilecount="221" columns="17">
+ <image source="../img/map/path.png" width="276" height="216"/>
+</tileset>
